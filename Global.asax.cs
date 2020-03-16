a42f978b709c4d729ae10186cdb2dcc019984ec8
Georgi Georgiev <gigeorgiev@progress.com>
2020-03-16 11:54:13 +0200

    Upgrade Sitefinity to 13.0 BETA

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web;
+using System.Web.Helpers;
 using Telerik.Microsoft.Practices.Unity;
 using Telerik.Sitefinity.Abstractions;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing;
@@ -10,5 +11,11 @@ namespace SitefinityWebApp
 {
     public class Global : System.Web.HttpApplication
     {
+        protected void Application_Start()
+        {
+            // Temporary workaround due to issue: https://feedback.progress.com/Project/153/feedback/Details/263075-this-operation-requires-iis-integrated-pipeline-mode-error-when-using-antiforgery
+            // that affects the Live Chat screen in Administration
+            AntiForgeryConfig.SuppressXFrameOptionsHeader = true;
+        }
     }
 }
